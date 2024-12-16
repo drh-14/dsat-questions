45661eceecd2e1f1f63989e82dc82c1785082d40
drh-14 <darrenh1134@gmail.com>
2024-12-16 04:36:23 -0500

    difficulty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   const mathDomains =  ["Algebra", "Advanced Math", "Problem-Solving and Data Analysis", "Geometry and Trigonometry"];
   const [domain, setDomain] = useState("");
   const [skill, setSkill] = useState("");
+  const [difficulty, setDifficulty] = useState("");
   return (
     <main>
       <div className = 'flex items-center flex-col mt-20 text-4xl gap-8'>
@@ -27,6 +28,14 @@ export default function Home() {
         <FormControl className = 'w-2/12'>
           <InputLabel>Skill</InputLabel>
           <Select></Select>
+        </FormControl>
+        <FormControl className = 'w-2/12'>
+          <InputLabel>Difficulty</InputLabel>
+          <Select value = {difficulty}>
+            <MenuItem value = "Easy" onClick = {() => setDifficulty("Easy")}>Easy</MenuItem>
+            <MenuItem value = "Medium" onClick = {() => setDifficulty("Medium")}>Medium</MenuItem>
+            <MenuItem value = "Hard" onClick = {() => setDifficulty("Hard")}>Hard</MenuItem>
+          </Select>
         </FormControl>     
         <Button variant = "contained">Generate my Question!</Button>
       </div>
