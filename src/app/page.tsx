ba01cdec94ab9cb3fc27e152e6537444798672ea
drh-14 <darrenh1134@gmail.com>
2024-12-16 18:18:35 -0500

    added skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,49 @@ export default function Home() {
   const [domain, setDomain] = useState("");
   const [skill, setSkill] = useState("");
   const [difficulty, setDifficulty] = useState("");
+  const valid_reading_and_writing_skills: {[key:string]: string[]} = {
+    "": [],
+    "Information and Ideas": ["Central Ideas and Details", "Inferences", "Command of Evidence"], 
+    "Craft and Structure": ["Words in Context", "Text Structure and Purpose", "Cross-Text Connections"],
+    "Expression of Ideas": ["Rhetorical Synthesis", "Transitions"],
+    "Standard English Conventions": ["Boundaries", "Form, Structure, and Sense"]
+  };
+  const valid_math_skills: {[key:string]: string[]} = {
+  "": [],
+  "Algebra": 
+      ["Linear equations in one variable", "Linear functions", "Linear equations in two variables", 
+      "Systems of two linear equations in two variables", "Linear inequalities in one or two variables"
+      ], 
+  "Advanced Math": 
+      ["Nonlinear functions", "Nonlinear equations in one variable and systems of equations in two variables", "Equivalent expressions"], 
+       "Problem-Solving and Data Analysis": [
+      "Ratios, rates, proportional relationships, and units", "Percentages", 
+      "One-variable data: Distributions and measures of center and spread", "Two-variable data: Models and scatterplots", 
+      "Probability and conditional probability", "Inference from sample statistics and margin of error", 
+      "Evaluating statistical claims: Observational studies and experiments"
+      ], 
+  "Geometry and Trigonometry": ["Area and volume", "Lines, angles, and triangles", "Right triangles and trigonometry", "Circles"]
+   };
+
   return (
     <main>
       <div className = 'flex items-center flex-col mt-20 text-4xl gap-8'>
         <h1>Welcome to DSAT-Questions</h1>
-        <h2 className = 'text-2xl mb-20'>To get started, pick the subject, domain, and skill.</h2>
+        <h2 className = 'text-2xl mb-20'>To get started, pick the subject, domain, skill, and difficulty.</h2>
         <FormControl className = 'w-2/12'>
           <InputLabel>Subject</InputLabel>
           <Select value = {subject}>
-          <MenuItem value = "Math" onClick  ={() => {setSubject("Math")}}>Math</MenuItem>
-          <MenuItem value = "Reading/Writing" onClick = {() => setSubject("Reading/Writing")}>Reading/Writing</MenuItem></Select>
+          <MenuItem value = "Math" onClick  ={() => {
+            setSubject("Math");
+            setDomain("");
+            setSkill("");
+
+          }}>Math</MenuItem>
+          <MenuItem value = "Reading/Writing" onClick = {() => 
+            {setSubject("Reading/Writing");
+             setDomain("");
+             setSkill("");
+            }}>Reading/Writing</MenuItem></Select>
         </FormControl>
         <FormControl className = 'w-2/12'>
           <InputLabel>Domain</InputLabel>
@@ -27,7 +60,10 @@ export default function Home() {
         </FormControl>
         <FormControl className = 'w-2/12'>
           <InputLabel>Skill</InputLabel>
-          <Select></Select>
+          <Select value = {skill}>
+            {(subject === "Reading/Writing") ? valid_reading_and_writing_skills[domain].map((text) => <MenuItem value = {text} key ={text} onClick = {() => setSkill(text)}>{text}</MenuItem>): 
+            valid_math_skills[domain].map((text) => <MenuItem value = {text} key = {text} onClick = {() => setSkill(text)}>{text}</MenuItem>)}
+          </Select>
         </FormControl>
         <FormControl className = 'w-2/12'>
           <InputLabel>Difficulty</InputLabel>
