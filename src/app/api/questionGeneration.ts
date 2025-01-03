a4e5dd38890293d3810dfa3722c8411d33e7efb4
drh-14 <darrenh1134@gmail.com>
2025-01-02 19:39:11 -0500

    removed langchain and pinecone

--- src/app/api/questionGeneration.ts
+++ src/app/api/questionGeneration.ts
@@ -1,16 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { PineconeStore } from "@langchain/pinecone";
-import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
-import { ChatOpenAI } from "@langchain/openai";
 import dotenv from "dotenv";
 
 dotenv.config();
-const pinecone = new PineconeClient();
-const pineconeIndex = pinecone.Index("DSAT-Questions");
-const embeddings = new OpenAIEmbeddings({model: "text-embedding-3-large"});
-const vecStore = await PineconeStore.fromExistingIndex(embeddings, {pineconeIndex, maxConcurrency: 5});
-const llm = new ChatOpenAI({model: "gpt-4o-mini", temperature: 0});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     if(req.method === 'POST'){
@@ -19,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const domain = data["domain"];
         const skill = data["skill"];
         const difficulty = data["difficulty"];
-        const prompt = `Based on similar questions, please generate a question with the following parameters:
+        const prompt = `Please generate a question with the following parameters:
          subject = ${subject}, domain = ${domain}, skill = ${skill}, difficulty = ${difficulty}. You can either generate the
          question with or without a diagram, but make it similar to questions in the specified category 
          and skill. For instance, if questions in the particular skill do not contain diagrams, do not
